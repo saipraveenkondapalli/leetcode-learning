9cc92175f531f1248f5f72089548a21a811e0874
Sai Praveen Kondapalli <63105626+saipraveenkondapalli@users.noreply.github.com>
2023-03-13 11:30:33 +0530

    moved some files to development

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,10 @@
-import os , sys
+import os
+import sys
 from project.app import app
 
 
 # Add the project directory to the sys.path
 sys.path.append((os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))))
-
-from app import app
-
 
 if __name__ == "__main__":
     app.run()
